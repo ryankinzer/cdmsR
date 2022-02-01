66ab172710d95e6e9324e73c31062fa24ff79fc3
Tyler Stright <tylers@nezpercetribe.org>
2022-02-01 09:59:48 -0700

    added arguments to P4 data grab.

--- R/getP4data.R
+++ R/getP4data.R
@@ -14,18 +14,49 @@
 #'
 #' @return NULL
 
-getP4data <- function(migration_year,  event_site = c('All', 'IMNTRP', 'JOHTRP', 'LOLTRP', 'SECTRP', 'SFCTRP'),
-                          cdms_host = 'https://npt-cdms.nezperce.org'){
+getP4data <- function(MRRProject = c('All', 'CDR', 'JLV', 'IMN', 'NPC', 'SCS'),
+                      EventSite = c('All', 'IMNTRP', 'JOHTRP', 'LOLTRP', 'SECTRP', 'SFCTRP'),
+                      EventType = c('All', 'Recapture', 'Mark', 'Recovery', 'Tally', 'Passive Recapture'),
+                      SRRcode = NULL,
+                      MigrationYear = NULL,
+                      BroodYear = NULL,
+                      CalendarYear = NULL,
+                      cdms_host = 'https://npt-cdms.nezperce.org'){
 
-  # Throw errors
-  {if(is.null(migration_year))stop("migration_year must be provided (YYYY)")}
+  MRRProject <- match.arg(MRRProject)
+  EventSite <- match.arg(EventSite)
+  EventType <- match.arg(EventType)
 
-  if(event_site == 'All') { event_site <- NULL }
+  # errors
+  if(!is.null(MigrationYear)) {
+    if(!grepl('\\d{4}', MigrationYear))stop("MigrationYear must be a 4-digit year (YYYY).")
+  }
+
+  if(!is.null(BroodYear)) {
+    if(!grepl('\\d{4}', BroodYear))stop("BroodYear must be a 4-digit year (YYYY).")
+  }
+
+  if(!is.null(CalendarYear)) {
+    if(!grepl('\\d{4}', CalendarYear))stop("CalendarYear must be a 4-digit year (YYYY).")
+  }
+
+  # set NULLs
+  if(MRRProject == 'All') { MRRProject <- NULL }
+  if(EventSite == 'All') { EventSite <- NULL }
+  if(EventType == 'All') { EventType <- NULL }
+
 
   # build URL for API
   req_url <- paste0(cdms_host,'/services/api/v1/npt/getp4data')
-  queryList <- list(Year = migration_year,
-                    EventSite = event_site)
+
+  queryList <- list(MRRProject = MRRProject,
+                    EventSite = EventSite,
+                    EventType = EventType,
+                    SRRcode = SRRcode,
+                    MigrationYear = MigrationYear,
+                    BroodYear = BroodYear,
+                    CalendarYear = CalendarYear)
+
 
   # httr::modify_url(req_url, query = queryList)
 
@@ -34,12 +65,12 @@ getP4data <- function(migration_year,  event_site = c('All', 'IMNTRP', 'JOHTRP',
                    query = queryList)
 
   httr::stop_for_status(req,
-                        task = paste0('query ', event_site, 'p4 records from migration year ', migration_year, '.'))
+                        task = paste0('query p4 records from CDMS'))
 
   # parse the response
   req_con <- httr::content(req, type = 'text', encoding = "UTF-8")
   df <- jsonlite::fromJSON(req_con, flatten = TRUE)
 
-  return(final_df)
+  return(df)
 
 }
